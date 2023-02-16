ada0615c8e553d4df6ac653cdf368b6e1eb8fdb0
QuiteCustard <56020602+QuiteCustard@users.noreply.github.com>
2023-02-16 15:59:02 +0000

    Font weight changes and remove title

--- front-page.php
+++ front-page.php
@@ -1,7 +1,6 @@
 <?php get_header(); ?>
 <main class="container">
     <?php while (have_posts()): the_post(); ?>
-    <h1><?= the_title(); ?></h1>
     <?= the_content() ?>
     <?php endwhile; ?>
 <?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <?php get_header(); ?>
-<main>
-    <h1>Latest posts</h1>
-    <div class="posts">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <article>
-                <?php the_post_thumbnail('large'); ?>
-                <?php the_category(); ?>
-                <h2><a href="<?= get_permalink() ?>"><?= the_title(); ?></a></h2>
-                <?php the_excerpt(); ?>
-        </article>
-        <?php endwhile; endif; ?>
-    </div>
+<main class="container">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <?php the_content(); ?>
+    <?php endwhile; endif; ?>
 <?php get_footer(); ?>
